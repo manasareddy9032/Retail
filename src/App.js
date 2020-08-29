4f5db93ea7e101934553676c8957aa6e2bc1cc48
Manasa Reddy <manasareddy9032@gmail.com>
2020-08-29 10:50:59 -0700

    Initila changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RetailBase from './containers/RetailBase';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RetailBase />
+    </div>
+  );
+}
+
+export default App;
